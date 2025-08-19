99960944009edeede12b2dcf5fd9a162af92577e
MatheusMartinsss <matheus.s.martins98@gmail.com>
2025-08-18 23:07:26 -0300

    config initial db connection

--- backend-api/src/config/data-source.ts
+++ backend-api/src/config/data-source.ts
@@ -1,16 +1,17 @@
+import "reflect-metadata";
 import { DataSource } from "typeorm";
 import dotenv from "dotenv";
 
 dotenv.config();
 
 export const AppDataSource = new DataSource({
-    type: "mysql", 
+    type: "mysql",
     host: process.env.DB_HOST,
     port: Number(process.env.DB_PORT),
     username: process.env.DB_USER,
     password: process.env.DB_PASS,
     database: process.env.DB_NAME,
-    synchronize: false, 
+    synchronize: false,
     logging: true,
     entities: [__dirname + "/../entities/*.{ts,js}"],
     migrations: [__dirname + "/../migrations/*.{ts,js}"],
--- backend-api/src/index.ts
+++ backend-api/src/index.ts
@@ -1,4 +1,5 @@
 import express, { Request, Response } from "express";
+import { AppDataSource } from "./config/data-source";
 
 const app = express();
 const PORT = process.env.PORT || 8081;
@@ -14,3 +15,13 @@ app.get("/", (req: Request, res: Response) => {
 app.listen(PORT, () => {
     console.log(`Servidor rodando em http://localhost:${PORT}`);
 });
+
+AppDataSource.initialize()
+    .then(() => {
+        console.log("📦 Banco conectado com sucesso!");
+
+        app.listen(3000, () => {
+            console.log("🚀 Servidor rodando na porta 3000");
+        });
+    })
+    .catch((error) => console.error("Erro ao conectar no banco:", error));
