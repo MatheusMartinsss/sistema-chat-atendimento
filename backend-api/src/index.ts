2a76b805914d00bdda50728382e9334374e99945
MatheusMartinsss <matheus.s.martins98@gmail.com>
2025-08-18 22:54:04 -0300

    initial index

---
+++ backend-api/src/index.ts
@@ -0,0 +1,16 @@
+import express, { Request, Response } from "express";
+
+const app = express();
+const PORT = process.env.PORT || 8081;
+
+
+app.use(express.json());
+
+
+app.get("/", (req: Request, res: Response) => {
+    res.send("Hello word");
+});
+
+app.listen(PORT, () => {
+    console.log(`Servidor rodando em http://localhost:${PORT}`);
+});
